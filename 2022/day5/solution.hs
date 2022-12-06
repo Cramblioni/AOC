1497f2bc2f76849e02b25df563c29bacc253ba82
Cramble <69367193+Cramblioni@users.noreply.github.com>
2022-12-06 02:37:52 +0000

    finished day 5

--- 2022/day5/solution.hs
+++ 2022/day5/solution.hs
@@ -1,5 +1,5 @@
 module Main where
-main = interact $ show . parse
+main = interact solution1
 
 data Program = Prog [[Char]] [Instr] deriving (Show)
 data Instr = Move Int Int Int deriving (Show)
@@ -10,18 +10,29 @@ chunk n xs = take n xs : chunk n (drop n xs)
 rotate ([]:_) = []
 rotate xs = map head xs : rotate (map tail xs)
 
-parseState = map (filter (/=' ')) . rotate . map (map (!!1)) . map (chunk 4)
+parseState = ([]:) . map (filter (/=' ')) . rotate . map (map (!!1)) . map (chunk 4)
 
 parseInstr = (\[n, s, d] -> Move n s d) . map read . map last . chunk 2 . words
 
 parse :: String -> Program
 parse = f [] . lines
-    where f acc ("": xs) = Prog (parseState . init $ acc) (map parseInstr xs)
+    where f acc ("": xs) = Prog (parseState . init . reverse$ acc)
+                              $ (map parseInstr xs)
           f acc (x: xs) = f (x:acc) xs
 
-step (Prog state (cur: curs)) = undefined
-    where pull n r a [] _   = (reverse a, r)
-          pull n r a (x:xs)   0 = pull n (take n x) (drop n x: a) xs (pred -1)
+step (Prog state ((Move n s d): curs)) = let
+            (istate, tmp) = pull n [] [] state s
+            fstate = put (tmp) [] istate d -- for part 1 (reverse tmp)
+        in Prog fstate curs
+    where pull n r a [] _       = (reverse a, r)
+          pull n r a (x:xs)   0 = pull n (take n x) (drop n x: a) xs (-1)
           pull n r a (x:xs) ind = pull n r (x:a) xs (pred ind)
-          put n v a [] = reverse a
-          put
+          put n a []     ind = reverse a
+          put n a (x:xs)   0 = put n ((n++x):a) xs (-1)
+          put n a (x:xs) ind = put n (x:a) xs (pred ind)
+
+run :: Program -> [[Char]]
+run (Prog x []) = x
+run (Prog s p)  = run $ step (Prog s p)
+
+solution1 = show . map head . tail . run . parse
