3361869f65a0774ac6b0f1daf77bcfd899250287
Cramble <69367193+Cramblioni@users.noreply.github.com>
2022-12-09 11:24:38 +0000

    fixed solution 2 day six after i broke it by shortening it (:

--- 2022/day6/solution.hs
+++ 2022/day6/solution.hs
@@ -6,9 +6,10 @@ unique = f []
     where f _ []       = True
           f acc (x:xs) | elem x acc = False
                        | otherwise  = f (x:acc) xs
-dosrann n x | length x < n = []
-            | otherwise    = take n x : dosran (tail x)
-
+dosran :: Int -> [a] -> [[a]]
+dosran n x | length x < n = []
+           | otherwise    = take n x : dosran n (tail x)
+solution1 :: [Char] -> String
 solution1 = show . fst . head . filter (unique . snd) . zip [4 ..] . dosran 4
-
+solution2 :: [Char] -> String
 solution2 = show . fst . head . filter (unique . snd) . zip [14 ..] . dosran 14
